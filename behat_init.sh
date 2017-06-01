0ee991d7a3cf3ad833f715c1d3535d333c12ddd9
Jake Dallimore <jake@moodle.com>
2017-06-01 14:19:29 +0800

    behat_init: Fix for firefox binary and chrome driver location
    Made changes so the firefox binary location and chromedriver location
    can be set, and not assumed nor hard coded.

--- behat_init.sh
+++ behat_init.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 # Simple behat browser runner.
-# This runs selenium with the relevant browser and framebuffering options, or phantom js.
+# This runs selenium with the relevant browser and framebuffering options (ff and chrome only)
+# or against the phantom js.
+#
+# Examples:
+#
+# behat_init 				<--(defaults to chrome and frame buffering on)
+# behat_init chrome 0 		<--(chrome with frame buffering off)
+# behat_init firefox 		<--(firefox with frame buffering on)
+# behat_init phantomjs 		<--(phantomjs, frame buffering not applicable)
 
 BROWSER="${1:-chrome}"
 XVFB="${2:-1}"
 
 # Keep track of the config, only for user display purposes.
-cfg=''
+cfg=""
 
-# Location of the selenium jar file.
+# Locations of some bits we need.
 SELENIUM_JAR="/opt/selenium-server-standalone-2.53.0.jar"
 PHANTOMJS_BIN="/opt/phantomjs"
+FIREFOX_BIN="/opt/Firefox-46.0.1/firefox-sdk/bin/firefox"
+CHROMEDRIVER_BIN="/usr/bin/chromedriver"
+# Note: Chromedriver expects /usr/bin/google-chrome as chrome location or symlink.
 
-# Base run command. We'll modify this based on $ args.
+# Base run command, modified by args.
 CMD="java -jar $SELENIUM_JAR"
 
+# Determine driver.
 if [ "$BROWSER" = 'firefox' ]; then
 	cfg=$cfg"Browser: Firefox\n"
-	CMD="$CMD -Dwebdriver.firefox.bin=\"/opt/Firefox-46.0.1/firefox-sdk/bin/firefox\""
+	CMD="$CMD -Dwebdriver.firefox.bin=\"$FIREFOX_BIN\""
 elif [ "$BROWSER" = 'phantomjs' ]; then
 	cfg=$cfg"Browser: PhantomJS\n"
 	CMD="$PHANTOMJS_BIN --webdriver=4444"
 else
 	cfg=$cfg"Browser: Chrome\n"
+	CMD="$CMD -Dwebdriver.chrome.driver=\"$CHROMEDRIVER_BIN\""
 fi
 
+# Determine frame buffering.
 if [ "$BROWSER" = "chrome" -o "$BROWSER" = "firefox" ]; then
 	if [ "$XVFB" = "1" ]; then
 		# Buffering enabled, so prepend the relevant xvfb-run statement to the run command.
@@ -34,6 +48,8 @@ if [ "$BROWSER" = "chrome" -o "$BROWSER" = "firefox" ]; then
 		cfg=$cfg"Frame buffering: Off\n"
 	fi
 fi
+
+# Output run config, kill any running servers, and run current config.
 cfg=$cfg"Command: $CMD\n"
 echo -e "---------------------\nRun config:\n$cfg---------------------\n"
 pkill -f xvfb-run
