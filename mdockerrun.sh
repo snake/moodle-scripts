158be17424d139875e6a983b3bb840e95a77f56f
Jake Dallimore <jake@moodle.com>
2017-12-08 10:22:20 +0800

    Runner script for the moodle docker suite

---
+++ mdockerrun.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Read in instance name
+# create path /var/www/html/INSTANCENAME
+# Pre-flight check - check for config-dist or something in the dir.
+# copy in the docker config file.
+# Now, set env vars and run the moodle docker.
+
+MOODLEDOCKERPATH="/home/jake/scripts/docker-moodle"
+MOODLEDOCKERCONFIGNAME="config.docker-template.php"
+INSTANCENAME="${1:-msmaster}"
+INSTANCEDB="${2:-mssql}"
+INSTANCEPATH="/var/www/html/$INSTANCENAME"
+
+# Sanity checks.
+if [ ! -f "$INSTANCEPATH/config-dist.php" ]
+then
+    echo "Instance '$INSTANCEPATH' could not be found. No config-dist.php found there."
+    exit 1
+fi
+if [ ! -f "$MOODLEDOCKERPATH/$MOODLEDOCKERCONFIGNAME" ]
+then
+    echo "Moodle docker directory '$MOODLEDOCKERPATH' could not be found. No $MOODLEDOCKERCONFIGNAME found there."
+    exit 1
+fi
+
+# Paths are valid, so copy the config.php into the dir.
+cp "$MOODLEDOCKERPATH/$MOODLEDOCKERCONFIGNAME" "$INSTANCEPATH/config.php"
+
+# Set up env vars.
+export MOODLE_DOCKER_WWWROOT="$INSTANCEPATH"
+export MOODLE_DOCKER_DB="$INSTANCEDB"
+
+# Run the docker.
+eval "$MOODLEDOCKERPATH/bin/moodle-docker-compose up -d"
+
+echo "Site up at http://localhost:8000"
